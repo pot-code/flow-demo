f6860e4401ef3f8bbf78be46ab0ccf81f0dbbe55
pot-code <136290354@qq.com>
2023-08-20 22:12:12 +0800

    feat: toast viewport style

--- src/components/toast/viewport.tsx
+++ src/components/toast/viewport.tsx
@@ -3,5 +3,5 @@ export interface ViewportProps {
 }
 
 export default function Viewport({ children }: ViewportProps) {
-  return <div className="fixed right-0 bottom-0 flex flex-col gap-unit-sm w-[372px] p-unit-md z-[999]">{children}</div>
+  return <div className="fixed right-0 bottom-0 flex flex-col gap-unit-sm w-[372px] p-unit-md z-1">{children}</div>
 }
