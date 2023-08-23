ddcf3b0649796c616ad16ac1d8316dda8ae03540
pot-code <136290354@qq.com>
2023-08-23 09:39:14 +0800

    feat: extract flow list query

---
+++ src/features/dashboard/use-flow-list-query.ts
@@ -0,0 +1,40 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { flowApi } from "@/api/flow"
+import { useToast } from "@/components/toast"
+import { HttpError } from "@/core/http/error"
+import { Time } from "@/util/duration"
+import { delayedPromise } from "@/util/promise"
+
+const cacheKey = "flow-list"
+
+export default function useFlowListQuery(enabled = true) {
+  const toast = useToast()
+  const queryClient = useQueryClient()
+  const query = useQuery(
+    [cacheKey],
+    () => delayedPromise(0.3 * Time.Second, flowApi.list)().then((res) => res.data.data),
+    {
+      enabled,
+      staleTime: 3 * Time.Minute,
+    },
+  )
+
+  const invalidateCache = useCallback(() => {
+    queryClient.invalidateQueries([cacheKey])
+  }, [queryClient])
+
+  useEffect(() => {
+    if (query.isError) {
+      toast.error("获取流程列表失败", {
+        description: (query.error as HttpError).message,
+      })
+    }
+  }, [query.error, query.isError, toast])
+
+  return {
+    isLoading: query.isLoading,
+    isRefreshing: query.isFetching,
+    data: query.data,
+    invalidateCache,
+  }
+}
