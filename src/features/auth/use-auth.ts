68abfc28d01079ce359c1509cddbae8f57ff417c
pot-code <136290354@qq.com>
2023-08-25 21:11:40 +0800

    refactor: use-auth

--- src/features/auth/use-auth.ts
+++ src/features/auth/use-auth.ts
@@ -1,17 +1,10 @@
 import { useQuery } from "@tanstack/react-query"
 import { authApi } from "@/api/auth"
 import useAuthStore from "./use-auth-store"
-import { HttpError } from "@/core/http/error"
 
 export default function useAuth() {
   const { isAuthenticated, setIsAuthenticated } = useAuthStore()
-  const { isLoading, isSuccess, isError, error } = useQuery(["is-authenticated"], () => authApi.isAuthenticated())
-
-  useEffect(() => {
-    if (isError && (error as HttpError).code === 401) {
-      setIsAuthenticated(false)
-    }
-  }, [error, isError, setIsAuthenticated])
+  const { isLoading, isSuccess } = useQuery(["is-authenticated"], () => authApi.isAuthenticated())
 
   useEffect(() => {
     if (isSuccess) {
